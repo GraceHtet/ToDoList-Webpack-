138438f80440c10ca51c29d44c79fa9cf52dd8b5
Thiri <thirihtet246@gmail.com>
2023-01-18 15:44:13 +0630

    fixed linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-disable */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    bundle: path.resolve(__dirname, './src/index.js'),
+    bundle: path.resolve(__dirname, "./src/index.js"),
   },
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'To do List',
-      inject: false,
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'src', 'index.html'),
+      title: "To do List",
+      filename: "index.html",
+      template: path.resolve(__dirname, "src", "index.html"),
     }),
   ],
   output: {
-    filename: '[name][contenthash].js',
-    path: path.resolve(__dirname, './dist'),
-    assetModuleFilename: 'assets/img/[name][ext]',
+    filename: "main.js",
+    path: path.resolve(__dirname, "./dist"),
+    assetModuleFilename: "./assets/img/[name][ext]",
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
+            loader: "html-loader",
           },
         ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
