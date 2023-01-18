360f06babf9a600febe4ff49183b5c34b484d0b0
Thiri <thirihtet246@gmail.com>
2023-01-18 13:52:08 +0630

    FIxed linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,40 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { Z_PARTIAL_FLUSH } = require("zlib");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "./dist"),
-    assetModuleFilename: "assets/img/[name][ext]",
+    filename: 'main.js',
+    path: path.resolve(__dirname, './dist'),
+    assetModuleFilename: 'assets/img/[name][ext]',
     clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
           },
         ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
