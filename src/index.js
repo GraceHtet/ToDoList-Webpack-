15759896430285af2635adf02d6faee2512014cf
Thiri <thirihtet246@gmail.com>
2023-01-20 12:27:06 +0630

    Add list functions
    Added add, remove, edit function
    Edited style

--- src/index.js
+++ src/index.js
@@ -1,50 +1,141 @@
 import './style.css';
-import square from './assets/img/square.svg';
-import moreDots from './assets/img/more-vertical.svg';
+import './assets/img/square.svg';
+import './assets/img/more-vertical.svg';
+import './assets/img/bin.svg';
 
-const lists = [
-  {
-    description: 'wash the dishes',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'sweep the floor',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'clean the room',
-    completed: false,
-    index: 2,
-  },
-];
-
+// add
 const todoEl = document.querySelector('.todo-list');
 
-const addList = (desc) => {
-  const lists = document.createElement('div');
-  lists.classList = 'lists';
-  const checkBox = document.createElement('img');
-  checkBox.src = square;
-  checkBox.classList = 'check-box';
-  checkBox.alt = 'square';
+class Lists {
+  constructor() {
+    this.tdlists = JSON.parse(localStorage.getItem('tdlists')) || [];
+  }
 
-  const todoText = document.createElement('input');
-  todoText.classList = 'todo-text';
-  todoText.value = desc;
-  todoText.disabled = true;
+  delList(item, idx) {
+    this.tdlists = JSON.parse(localStorage.getItem('tdlists')) || [];
+    const tdIdx = this.tdlists[idx].index;
+    this.tdlists.splice(tdIdx, 1);
+    this.tdlists.forEach((tdlist, tdnum) => {
+      tdlist.index = tdnum;
+    });
 
-  const more = document.createElement('img');
-  more.src = moreDots;
-  more.classList = 'more';
-  more.alt = 'more-vertical';
+    localStorage.setItem('tdlists', JSON.stringify(this.tdlists));
+    this.showList();
+  }
 
-  lists.append(checkBox, todoText, more);
+  showList() {
+    todoEl.innerHTML = '';
+    this.tdlists.forEach((tdlist) => {
+      const lists = document.createElement('div');
+      lists.classList = 'lists';
+      const checkBox = document.createElement('img');
+      checkBox.src = './assets/img/square.svg';
+      checkBox.classList = 'check-box';
+      checkBox.alt = 'square';
 
-  todoEl.appendChild(lists);
+      const todoText = document.createElement('input');
+      todoText.type = 'text';
+      todoText.name = 'text';
+      todoText.classList = 'todo-text';
+      todoText.value = tdlist.description;
+      todoText.disabled = true;
+
+      const more = document.createElement('img');
+      more.src = './assets/img/more-vertical.svg';
+      more.classList = 'more';
+      more.alt = 'more-vertical';
+
+      lists.append(checkBox, todoText, more);
+      todoEl.appendChild(lists);
+    });
+  }
+
+  storeLocal(td) {
+    this.tdlists.push({
+      description: td,
+      completed: false,
+      index: this.tdlists.length,
+    });
+    localStorage.setItem('tdlists', JSON.stringify(this.tdlists));
+  }
+
+  edit(val, idx) {
+    this.tdlists = JSON.parse(localStorage.getItem('tdlists')) || [];
+    this.tdlists[idx].description = val;
+    localStorage.setItem('tdlists', JSON.stringify(this.tdlists));
+  }
+}
+
+const listFuns = new Lists();
+listFuns.showList();
+
+// remove
+const textnormal = (item) => {
+  item.parentElement.style.background = '#fff';
+  item.nextElementSibling.src = './assets/img/more-vertical.svg';
+  item.nextElementSibling.className = 'more';
+  item.disabled = true;
 };
 
-lists.forEach((list) => {
-  addList(list.description);
-});
+const removeTodo = () => {
+  todoEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    const cur = e.target;
+    const curPrev = cur.previousElementSibling;
+    const curlist = cur.parentElement;
+    const curIdx = Array.from(todoEl.childNodes).indexOf(curlist);
+
+    if (cur.className.includes('more')) {
+      curlist.classList.add('active');
+      curlist.style.background = '#F8EDE3';
+      cur.src = './assets/img/bin.svg';
+      cur.className = 'bin';
+      curPrev.disabled = false;
+    } else if (cur.className.includes('bin')) {
+      listFuns.delList(curlist, curIdx);
+    }
+
+    if (curlist.lastElementChild.className === 'bin') {
+      if (cur.className === 'todo-text') {
+        curlist.addEventListener('focusout', () => {
+          textnormal(cur);
+        });
+      }
+    }
+  });
+};
+
+removeTodo();
+
+const editTodo = () => {
+  const todoEl = document.querySelector('.todo-list');
+  const todoTexts = todoEl.querySelectorAll('.todo-text');
+  todoTexts.forEach((text, idx) => {
+    text.onchange = () => {
+      listFuns.edit(text.value, idx);
+    };
+
+    text.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter' || e.key === 'Tab') textnormal(text);
+    });
+  });
+};
+
+// submit
+const todoInput = document.querySelector('.todo-input');
+
+const submitTodo = () => {
+  todoInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter' || e.key === 'Tab') {
+      listFuns.storeLocal(todoInput.value);
+      todoInput.value = '';
+      listFuns.showList();
+    }
+
+    editTodo();
+  });
+};
+
+submitTodo();
+
+editTodo();
